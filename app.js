5b95d2d7ffef4b17e0b91f7bffe05e025eac2d57
Monique <monique.meas@gmail.com>
2022-07-29 22:55:29 -0400

    add questions and answer

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const fs = require("fs");
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+//const fs = require("fs");
+//const generatePage = require('./src/page-template');
+//const pageHTML = generatePage(nam, github);
+//fs.writeFile('./index.htm', pageHJTML, err => {
+   // if (err) throw err;
 
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if(err) throw err;
-
-    console.log('Portfolio Completed! Check out index.html to the ouput!');
-});
-
-
+   // console.log('Portfolio Completed! Check out index.html to see the output!');
+//});
+inquirer
+.prompt([
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+}
+])
+.then(answer => console.log(answer));
